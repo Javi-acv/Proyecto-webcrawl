3a27d26a5d1238d129f4aa2b01373a0a7cec3ae9
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2024-11-20 18:49:12 -0600

    testing send emails

--- mails/send_mail.py
+++ mails/send_mail.py
@@ -4,9 +4,72 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def enviar_correo():
-    #Configurar correo 
-    server=smtplib.SMTP('smtp.gmail.com',587)
-    server.ehlo()
-    server.starttls()
-    server.login('proyectowebcrawler@gmail.com','xieg alws eejs ixzf')
-    
+    try:
+        #Mail settings
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.ehlo()
+        server.starttls()
+        server.login('proyectowebcrawler@gmail.com','xieg alws eejs ixzf')
+        
+        mensaje= MIMEMultipart()
+        mensaje['From']='WebCrawler'
+        mensaje['To']='alvaritodavilag0331@gmail.com'
+        mensaje['Subject']='Resultados de búsqueda'
+        
+        #read json file
+        with open('crawl/test.json','r',encoding='utf-8') as f:
+            contenido=json.load(f)
+        
+        #body mail
+        contenido_correo="""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <style>
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+                th, td {
+                    border: 1px solid black;
+                    padding: 8px;
+                    text-align: left;
+                }
+                th {
+                    background-color: #f2f2f2;
+                }
+            </style>
+        </head>
+        <body>
+            <h2>Listado de conciertos</h2>
+            <table>
+                <tr>
+                    <th>Título</th>
+                    <th>Precio</th>
+                </tr>
+        """
+
+        for libro in contenido:
+            contenido_correo += f"""
+                <tr>
+                    <td>{libro['title']}</td>
+                    <td>{libro['price']}</td>
+                </tr>
+            """
+
+        contenido_correo += """
+            </table>
+        </body>
+        </html>
+        """
+
+        mensaje.attach(MIMEText(contenido_correo,'html'))
+        #send mail
+        server.sendmail('proyectowebcrawler@gmail.com','alvaritodavilag0331@gmail.com',mensaje.as_string())
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
+    finally:
+        server.quit()
+
+#test send mail
+enviar_correo()
