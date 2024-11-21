b9d03251e3b5d788157e75004542d0f79a8591bd
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2024-11-21 03:16:04 -0600

    Last details

--- crawl/crawl/spiders/Spider.py
+++ crawl/crawl/spiders/Spider.py
@@ -24,13 +24,34 @@ class SongkickSpider(CrawlSpider):
         },
         "DOWNLOAD_DELAY": 3,
     }
+    
+    def parse_start_url(self, response):
+        """
+        Procesa la página inicial directamente.
+        """
+        self.logger.info("Procesando la página inicial: %s", response.url)
+        
+        # Extraer todos los eventos en la página inicial
+        events = response.css('li.event-listings-element')
+
+        for event in events:
+            yield {
+                "Artista": event.css('p.artists a.event-link strong::text').get(),
+                "Lugar": event.css('p.location a.venue-link::text').get(),
+                "Ciudad": event.css('p.location span.city-name::text').get(),
+                "Fecha": event.css('time::attr(datetime)').get(),
+                "Enlace": response.urljoin(event.css('p.artists a.event-link::attr(href)').get()),
+            }
 
     def parse_event(self, response):
+        """
+        Mantén esta función si deseas procesar páginas de eventos individuales.
+        """
         self.logger.info("Procesando evento: %s", response.url)
         yield {
             "Artista": response.css('div.artists-venue-location-wrapper p.artists strong::text').get(),
             "Lugar": response.css('div.artists-venue-location-wrapper p.location a.venue-link::text').get(),
             "Ciudad": response.css('div.artists-venue-location-wrapper p.location span.city-name::text').get(),
             "Fecha": response.css('li.date-element time::attr(datetime)').get(),
-            "Enlace": response.url
+            "Enlace": response.url,
         }
